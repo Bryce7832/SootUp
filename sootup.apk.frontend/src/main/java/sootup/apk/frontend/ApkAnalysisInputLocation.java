aad7f72240bd1605a6a35d82262f9a2802417c8e
Bryce7832 <bryce.hojo@gmail.com>
2024-09-19 15:57:20 -0700

    Added a explicit use of return type

--- sootup.apk.frontend/src/main/java/sootup/apk/frontend/ApkAnalysisInputLocation.java
+++ sootup.apk.frontend/src/main/java/sootup/apk/frontend/ApkAnalysisInputLocation.java
@@ -106,7 +106,7 @@ public class ApkAnalysisInputLocation implements AnalysisInputLocation {
   @Override
   public Collection<? extends SootClassSource> getClassSources(@Nonnull View view) {
     return classNamesList.entrySet().stream()
-        .flatMap(
+        .<SootClassSource>flatMap(
             className ->
                 StreamUtils.optionalToStream(
                     getClassSource(
